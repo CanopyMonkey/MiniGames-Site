885c2e582263361b6e487ba8985660b314231f92
khaiden <CanopyMonkey>
2025-06-23 16:21:09 -0600

    fixing dns glitch

--- index.js
+++ index.js
@@ -1,27 +1,14 @@
-
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = 3001;
 
-app.use(express.static('public'));
+// Serve static files under '/MiniGames-Site'
+app.use('/MiniGames-Site', express.static(path.join(__dirname, 'public')));
+
+// Optional root route fallback
 app.get('/', (req, res) => {
-    res.send('Sorry, there was an error.');
+    res.send('Go to <a href="/MiniGames-Site/index.html">/MiniGames-Site/index.html</a>');
 });
 
-app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
-
-//
-/*const execSync = require('child_process').execSync;
-// import { execSync } from 'child_process';  // replace ^ if using ES modules
-
-const output = execSync('ls', { encoding: 'utf-8' });  // the default is 'buffer'
-console.log('Output was:\n', output);*/
-//window.open('localhost:3000');
-//<a href="https://www.codexworld.com" target="_blank">Visit CodexWorld</a>
-//window.open('https://www.codexworld.com', '_blank');
-/*if (typeof window !== 'undefined') {
-    console.log('You are on the browser,You are good to go')
-    } else {
-    console.log('You are on the server,Cannot execute')
-   }*/
-//app.server.close;
+app.listen(PORT, () => console.log(`Server listening on http://localhost:${PORT}/MiniGames-Site/`));
